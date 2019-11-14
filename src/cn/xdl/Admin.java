6710607b69eab4a2e82ede4ac64c4f0721119cd4
wei1314521 <502790415@qq.com>
2019-11-14 10:13:13 +0800

    10.13

--- src/cn/xdl/Admin.java
+++ src/cn/xdl/Admin.java
@@ -1,11 +1,9 @@
 package cn.xdl;
-
 public class Admin {
-
 	public static void main(String[] args) {
 		System.out.println("Admin");
 		System.out.println("Admin");
-
+		System.out.println("Admin");
+		System.out.println("Admin");
 	}
-
 }
