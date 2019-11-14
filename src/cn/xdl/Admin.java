32be21222a95d00965e7a9fb943a8f09a3ba0da4
wei1314521 <502790415@qq.com>
2019-11-14 10:07:08 +0800

    10.07

---
+++ src/cn/xdl/Admin.java
@@ -0,0 +1,10 @@
+package cn.xdl;
+
+public class Admin {
+
+	public static void main(String[] args) {
+		System.out.println("Admin");
+
+	}
+
+}
---
+++ src/cn/xdl/User.java
@@ -0,0 +1,10 @@
+package cn.xdl;
+
+public class User {
+
+	public static void main(String[] args) {
+		System.out.println("User");
+
+	}
+
+}
